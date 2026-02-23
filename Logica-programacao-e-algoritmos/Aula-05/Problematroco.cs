3d3eea77ad42c763c672cebb132240c7cd50a122
Matheus797-pgn <matheushenriqueramiro12345@gmail.com>
2026-02-23 19:17:02 -0300

    SEM TRY E CATCH

--- Logica-programacao-e-algoritmos/Aula-05/Problematroco.cs
+++ Logica-programacao-e-algoritmos/Aula-05/Problematroco.cs
@@ -1,12 +1,12 @@
 using static System.Console;
 double Precounitario, Quantidade, Dinheirorecebido, troco;
-Console.WriteLine("preco do produto é");
+WriteLine("preco do produto é");
 Precounitario = double.Parse(ReadLine());
-Console.WriteLine("a quantidade é");
+WriteLine("a quantidade é");
 Quantidade = double.Parse(ReadLine());
-Console.WriteLine("O dinheiro recebido é");
+WriteLine("O dinheiro recebido é");
 Dinheirorecebido = double.Parse(ReadLine());
 //processamento de dados
 troco = Dinheirorecebido-(Precounitario*Quantidade);
 // saida de dados
-Console.WriteLine($"o seu troco é {troco}");
+WriteLine($"o seu troco é {troco}");
